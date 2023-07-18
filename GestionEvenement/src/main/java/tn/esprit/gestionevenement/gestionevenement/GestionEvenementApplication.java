71816e17177ec5e6c8da516c7cff49b13333191e
yessmine marzouk <yasmine.marzouk@avaxia-group.com>
2023-07-18 16:43:20 +0100

    yasmineMarzouk

--- GestionEvenement/src/main/java/tn/esprit/gestionevenement/gestionevenement/GestionEvenementApplication.java
+++ GestionEvenement/src/main/java/tn/esprit/gestionevenement/gestionevenement/GestionEvenementApplication.java
@@ -2,6 +2,9 @@ package tn.esprit.gestionevenement.gestionevenement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class GestionEvenementApplication {
@@ -9,5 +12,18 @@ public class GestionEvenementApplication {
     public static void main(String[] args) {
         SpringApplication.run(GestionEvenementApplication.class, args);
     }
+
+    @Bean
+    public WebMvcConfigurer configure() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry reg) {
+                reg.addMapping("/**").allowedOrigins("*");
+            }
+        };
+    }
+
+
+
 
 }
